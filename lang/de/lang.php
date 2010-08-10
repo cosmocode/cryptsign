22c4c2066af10d4b4a1f349a48b068f480729324
Adrian Lang <lang@cosmocode.de>
2010-08-10 15:15:50 +0200

    Add german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['valid'] = 'Gültige Signatur von %s.';
+$lang['invalid'] = 'Ungültige Signatur von %s.';
+
+$lang['js']['prompt'] = 'Text zum signieren eingeben:';
+$lang['js']['button'] = 'Einen Text signieren';
+$lang['js']['summary'] = 'signiert';
