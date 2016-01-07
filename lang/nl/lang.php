e29b440c25632a19751b7680c527d1097beacced
Vincent Kersten <v.m.e.kersten@braincarta.net>
2016-01-07 15:27:38 +0100

    Typo in translation

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -4,5 +4,5 @@ $lang['valid'] = 'Geldige digitale handtekening van %s.';
 $lang['invalid'] = 'Ongeldige digitale handtekening voor %s.';
 
 $lang['js']['prompt'] = 'Voer tekst in om digitaal te tekenen:';
-$lang['js']['button'] = 'Tekst digitaal onderteken';
+$lang['js']['button'] = 'Tekst digitaal ondertekenen';
 $lang['js']['summary'] = 'Digitaal ondertekend';
